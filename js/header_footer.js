eb6bdb907da7c32233282e629d3b5a294d2e9fce
Ken-Yoshinaga1 <k8963813@gmail.com>
2026-07-05 23:38:36 +0900

    a

---
+++ js/header_footer.js
@@ -0,0 +1,15 @@
+const loadHeader = async () => {
+  const res = await fetch("/components/header.html");
+  const html = await res.text();
+  document.getElementById("header").innerHTML = html;
+};
+
+loadHeader();
+
+const loadFooter = async () => {
+  const res = await fetch("/components/footer.html");
+  const html = await res.text();
+  document.getElementById("footer").innerHTML = html;
+};
+
+loadFooter();
